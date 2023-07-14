a50b605bf0677254e2cc8fdbd12c98f6a37d44cf
pieterherman-dev <identity@pieterherman.dev>
2023-07-14 07:54:44 +0200

    Time is now updated every second.

--- src/utils/timeUtils.ts
+++ src/utils/timeUtils.ts
@@ -25,10 +25,12 @@ const monthNames = [
 export const getFormattedTime = (date: Date): string => {
   const hours = date.getHours();
   const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
   return `${hours < 10 ? '0' + hours : hours}:${
     minutes < 10 ? '0' + minutes : minutes
-  }`;
+  }:${seconds < 10 ? '0' + seconds : seconds}`;
 };
+
 
 export const getFormattedDate = (date: Date): string => {
   const day = date.getDay();
